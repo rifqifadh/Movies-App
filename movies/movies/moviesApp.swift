94710513938a3bbcb9c97046eee29045115c4b3c
telkomdev-rifqi <rifqi.fadhlillah@gmail.com>
2021-09-29 20:52:49 +0700

    🎉

---
+++ movies/movies/moviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  moviesApp.swift
+//  movies
+//
+//  Created by Rifqi Fadhlillah on 29/09/21.
+//
+
+import SwiftUI
+
+@main
+struct moviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
